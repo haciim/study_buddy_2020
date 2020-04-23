a390022096eb3b35b61b7da72a28cc60ee5ca6c1
jamieboy1337 <jamieboy1337@gmail.com>
2020-04-22 22:50:18 -0700

    added some funky text to the main activity

--- app/src/main/java/com/example/studdybuddy/MainActivity.java
+++ app/src/main/java/com/example/studdybuddy/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.studdybuddy;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // test
+        setContentView(R.layout.activity_main);
+        System.out.println("hello world");
+    }
 }
