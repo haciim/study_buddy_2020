8b0e117d4c413ebc4b1c60c23240d83d4fbc86e0
jamieboy1337 <jamieboy1337@gmail.com>
2020-05-08 15:08:26 -0700

    modifications to test output

--- app/src/test/java/TimerRunnerTests.java
+++ app/src/test/java/TimerRunnerTests.java
@@ -19,6 +19,7 @@ public class TimerRunnerTests {
     private static SessionTimerCallback callback = new SessionTimerCallback() {
         public void callbackFunc(long secondsPassed) {
             System.out.println("called");
+            System.out.println(secondsPassed + " seconds");
             // figure out which second should be called
             // gauge offset based on that (like keep it under 100 ms off i guess)
             long diff = (System.currentTimeMillis() - currentTime);
@@ -27,31 +28,37 @@ public class TimerRunnerTests {
             // figure out offset
             long offset = Math.abs(diff - (secondsPassed * 1000));
             // stay roughly synchronized
-            System.out.println(offset);
+            System.out.println("offset (ms): " + offset);
             Assert.assertTrue(offset < 100);
 
         }
     };
 
+    private static final int MAX_CALLBACKS = 5;
+
     @Test
     public void TestTimerRunner() throws InterruptedException {
-        currentTime = System.currentTimeMillis();
         Handler handler = Mockito.mock(Handler.class);
         TimerRunner runner = new TimerRunner(handler);
-        runner.setCallback(callback);
-        runner.setStartTime(currentTime);
         final AtomicInteger runs = new AtomicInteger(0);
         Mockito.when(handler.postDelayed(Mockito.eq(runner), Mockito.anyLong())).then(new Answer<Void>() {
             public Void answer(InvocationOnMock invoc) throws InterruptedException {
-                // fake postDelayed
-                if (runs.incrementAndGet() < 3) {
-                    Thread.sleep((long)invoc.getArguments()[1]);
+                // fake postDelayed with callback cutoff
+                // in implementation this cutoff will be handled by cancelling the callback via the handler
+                if (runs.incrementAndGet() < MAX_CALLBACKS) {
+                    Thread.sleep((long) invoc.getArguments()[1]);
                     runner.run();
                 }
                 return null;
             }
         });
 
+        currentTime = System.currentTimeMillis();
+        runner.setCallback(callback);
+        runner.setStartTime(currentTime);
+
+        // in code: use handler.post instead
+        // this just ensures that our function runs only as long as the runner is active
         runner.run();
     }
 }
