02ccfcec2e689d470d988f3d5e4cd82c65ae0b8a
haciim <63818532+haciim@users.noreply.github.com>
2020-05-11 18:06:58 -0700

    Main screen (#6)
    * pushing_new_branch
    
    * Main screen now starts new activity, specified in MainActivity.java
    
    * fixed build issue
    
    authored-by: Micah Verwey <haciim@cs.washtington.edu>

--- app/src/main/java/studyBuddy/MainActivity.java
+++ app/src/main/java/studyBuddy/MainActivity.java
@@ -1,16 +1,43 @@
 package studyBuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.example.studdybuddy.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements View.OnClickListener {
 
+    private CardView newSession;
     @Override
+    /**
+     * This method called when the app is opened.
+     * Any data retrieval should start here.
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_layout);
+        setContentView(R.layout.home_layout);
+        newSession = findViewById(R.id.new_session_outer);
+        newSession.setOnClickListener(this);
+    }
+
+    @Override
+    /**
+     * This method is called when any clickable item
+     * in the home_layout is pressed.
+     * Each item results in a different action taken.
+     * @param view The view that called this method
+     */
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.new_session_outer:
+                Intent intent = new Intent(MainActivity.this, TestActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
