355b992d9eadb310d03ef81243c00d66f85e65b5
Tomi Tanaka <tomit@uw.edu>
2020-05-19 18:00:59 -0700

    set up fields in DataManager

--- app/src/main/java/studyBuddy/DataManager.java
+++ app/src/main/java/studyBuddy/DataManager.java
@@ -1,6 +1,10 @@
 package studyBuddy;
 
 public class DataManager {
+    private final String PET_PATH = "Pet.json";
+    private final String SESSION_PATH = "Sessions.json";
+
+
     public static void save() {
 
     }
