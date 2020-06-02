0732100a35b469e588397dafbae4bbdf635db8b5
jamieboy1337 <jamieboy1337@gmail.com>
2020-06-02 01:07:18 -0700

    added color changing to relevant drawables

--- app/src/main/java/studyBuddy/MainActivity.java
+++ app/src/main/java/studyBuddy/MainActivity.java
@@ -4,11 +4,14 @@ import android.app.AlarmManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -39,6 +42,28 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
         timeSelectorIsOpen = false;
+
+        ColorMatrixColorFilter filter = PrimaryColorPicker.getDayColorMatrixFilter(this);
+
+        ImageView sessionHistory = findViewById(R.id.session_history_inner);
+        sessionHistory.getDrawable().mutate().setColorFilter(filter);
+
+        ImageView circle = findViewById(R.id.circle_button_inner);
+        circle.getDrawable().mutate().setColorFilter(filter);
+
+        TextView info = findViewById(R.id.info_button_text);
+        info.setTextColor(PrimaryColorPicker.getDayColorInt(this));
+
+        TextView newSessionText = findViewById(R.id.newSession);
+        newSessionText.setTextColor(PrimaryColorPicker.getDayColorInt(this));
+
+        // https://stackoverflow.com/questions/22192291/how-to-change-the-status-bar-color-in-android
+        Window window = this.getWindow();
+
+        window.setStatusBarColor(PrimaryColorPicker.getDayColorInt(this));
+
+        ImageView background = findViewById(R.id.main_background);
+        background.getDrawable().mutate().setColorFilter(filter);
 
         // see if we need to open the activity back up
         Intent appIntent = getIntent();
